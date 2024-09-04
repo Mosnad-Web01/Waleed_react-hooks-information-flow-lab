a112e0db88a7cfb9819d3d0356c20c3187a63346
waleed7333 <waleed733150@gmail.com>
2024-09-04 09:16:20 +0300

    update JS files

--- src/__tests__/ShoppingList.test.js
+++ src/__tests__/ShoppingList.test.js
@@ -11,24 +11,51 @@ const testData = [
 ];
 
 test("displays all items when initially rendered", () => {
-  const { container } = render(<ShoppingList items={testData} />);
+  const { container } = render(
+    <ShoppingList
+      items={testData}
+      selectedCategory="All"
+      onCategoryChange={() => {}}
+    />
+  );
   expect(container.querySelector(".Items").children).toHaveLength(
     testData.length
   );
 });
 
 test("displays only items that match the selected category", () => {
-  const { container } = render(<ShoppingList items={testData} />);
+  const { rerender, container } = render(
+    <ShoppingList
+      items={testData}
+      selectedCategory="All"
+      onCategoryChange={() => {}}
+    />
+  );
 
   fireEvent.change(screen.getByRole("combobox"), {
     target: { value: "Dairy" },
   });
 
+  rerender(
+    <ShoppingList
+      items={testData}
+      selectedCategory="Dairy"
+      onCategoryChange={() => {}}
+    />
+  );
   expect(container.querySelector(".Items").children).toHaveLength(2);
 
   fireEvent.change(screen.getByRole("combobox"), {
     target: { value: "Dessert" },
   });
+
+  rerender(
+    <ShoppingList
+      items={testData}
+      selectedCategory="Dessert"
+      onCategoryChange={() => {}}
+    />
+  );
 
   expect(container.querySelector(".Items").children).toHaveLength(1);
 });
